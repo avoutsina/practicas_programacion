2d0a7060f7afb36edcfe961093dd8f01230977a5
Alejandro Voutsina <voutsina.alejandro@gmail.com>
2023-07-26 22:13:54 -0300

    tp listas

--- Clase_1/Curso_de_ingreso_PYTHON/ejercicios/07_tps/08_LISTAS_Conteo/app.py
+++ Clase_1/Curso_de_ingreso_PYTHON/ejercicios/07_tps/08_LISTAS_Conteo/app.py
@@ -44,11 +44,54 @@ class App(customtkinter.CTk):
         self.lista = []
 
     def btn_comenzar_ingreso_on_click(self):
-        pass
+        while True:
+            numero_solicitado=prompt("Por favor","ingrese un numero")
+            if numero_solicitado == None:
+                break
+            elif numero_solicitado.isdigit():
+                numero_solicitado=int(numero_solicitado)
+                self.lista.append(numero_solicitado)
+            elif numero_solicitado.isalpha() or numero_solicitado == '' or numero_solicitado.isalnum():
+                continue
+            elif int(numero_solicitado) < 0:
+                numero_solicitado = int(numero_solicitado)
+                self.lista.append(numero_solicitado)
+            else:
+                continue 
 
     def btn_mostrar_estadisticas_on_click(self):
-        pass
+        positivos = 0
+        negativos = 0
+        contador_positivos = 0
+        contador_negativos = 0
+        contador_ceros = 0
+        minimo_negativos = 0
+        maximo_positivos = 0
+        bandera_positivos = True
+        bandera_negativos = True
+        for numero in self.lista:
+            if numero > 0:
+                positivos += numero
+                contador_positivos += 1
+                if bandera_positivos == True or numero > maximo_positivos:
+                    bandera_positivos = False
+                    maximo_positivos = numero
+            elif numero < 0:
+                negativos += numero
+                contador_negativos += 1
+                if bandera_negativos == True or numero < minimo_negativos:
+                    bandera_negativos = False
+                    minimo_negativos = numero
+            else:
+                contador_ceros += 1
 
+        promedio_negativos = negativos / contador_negativos
+
+        alert("Titulo", f"La suma acumulada de los positivos es: {positivos} y la cantidad es: {contador_positivos}\nLa suma acumulada de los negativos es: {negativos} y la cantidad es: {contador_negativos}\nLa cantidad de ceros es: {contador_ceros}\nEl máximo de los positivos es: {maximo_positivos} y el minimo de los negativos es: {minimo_negativos}\nEl promedio de los negativos es: {promedio_negativos}")
+        
+        
+        
+        
 
 if __name__ == "__main__":
     app = App()
